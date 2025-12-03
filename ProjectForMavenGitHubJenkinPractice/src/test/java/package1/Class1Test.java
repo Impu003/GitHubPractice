9576388f441ef6a93f7241893a8e174495339b54
impus <impus@Impu>
2025-12-03 17:31:48 +0530

    My first commit

---
+++ ProjectForMavenGitHubJenkinPractice/src/test/java/package1/Class1Test.java
@@ -0,0 +1,14 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Class1Test {
+	
+	@Test
+	public void class1() {
+		
+		System.out.println("Hi**************");
+		
+	}
+	
+}
